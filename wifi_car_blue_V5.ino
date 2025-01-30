a5ef1ef2518c4d9dcad6546521668ceee89604c5
wydmynd <tomsofer@gmail.com>
2025-01-30 09:04:56 +0200

    works - but in non-joystick mode, turn directions are reversed

--- wifi_car_blue_V5.ino
+++ wifi_car_blue_V5.ino
@@ -6,7 +6,7 @@
 
 // Servo myservo;  // create servo object to control a servo
 
-
+const int JOYSTICK_RANGE = 100;
 
 const char *ssid = "WIFI_CAR_24";
 const char *password = "2456456456";
@@ -83,16 +83,16 @@ void setup() {
 
 
 void loop() {
-  server.handleClient();          // listen for HTTP requests from clients
+  server.handleClient();  // listen for HTTP requests from clients
 }
 
-void handleState()
-{
-  if (command!=prev_command)
-  {
-    prev_command=command;
+void handleState() {
+  if (command != prev_command) {
+    prev_command = command;
     Serial.print("DEBUG: State Command= ");
     Serial.println(command);
+
+
 
     if (command == "F") Forward();  // check string then call a function or set a value
     else if (command == "B") Backward();
@@ -103,8 +103,8 @@ void handleState()
     else if (command == "I") ForwardRight();
     else if (command == "J") BackwardRight();
     else if (command == "S") Stop();
-    else if (command == "V") BeepHorn();
-    else if (command == "v") BeepHorn(); //TODO add code to stop beeping horn
+    else if (command == "V") BeepHorn();       //TODO add code to beep horn
+    else if (command == "v") BeepHorn_stop();  //TODO add code to stop beeping horn
     else if (command == "W") TurnLightOn();
     else if (command == "w") TurnLightOff();
     else if (command == "0") SPEED = command.toInt() * 100;
@@ -118,31 +118,29 @@ void handleState()
     else if (command == "8") SPEED = command.toInt() * 100;
     else if (command == "9") SPEED = command.toInt() * 100;
     else if (command == "10") SPEED = command.toInt() * 100;
-  }
-  else
-  {
+  } else if (command == "") {
+    Serial.println("DEBUG: Error - Empty Command");
+    return;
+  } else {
     Serial.println("DEBUG: Repeat Command");
   }
 }
 
-void handleJoystick()
-{
-  int joystick_speed = (sqrt(pow(joystick_x,2)+pow(joystick_y,2)));
-  if (joystick_speed!=prev_joystick_speed)
-  {
-    prev_joystick_speed=joystick_speed;
+void handleJoystick() {
+  int joystick_speed = (sqrt(pow(joystick_x - JOYSTICK_RANGE, 2) + pow(joystick_y - JOYSTICK_RANGE, 2)));
+  if (joystick_speed != prev_joystick_speed) {
+    prev_joystick_speed = joystick_speed;
     Serial.print("DEBUG: Joystick_x=");
     Serial.print(joystick_x);
     Serial.print(",Joystick_y=");
     Serial.print(joystick_y);
     Serial.print(",Joystick Speed=");
     Serial.println(joystick_speed);
-    
-    move_with_joystick(); //move robot
+
+    move_with_joystick(joystick_x, joystick_y);  //move robot
   }
-  
-  else
-  {
+
+  else {
     Serial.println("DEBUG: Repeat Command");
   }
 }
@@ -150,23 +148,22 @@ void handleJoystick()
 // function prototypes for HTTP handlers
 void HTTP_handleRoot(void) {
   if (server.hasArg("State")) {
-     if (server.arg("State") != "") {
+    if (server.arg("State") != "") {
       command = server.arg("State");
       server.send(200, "text/html", "S");  // Send HTTP status 200 (Ok) and send some text to the browser/client
       handleState();
-     }
-  } else if (server.hasArg("JX")) { // check if the client sent a value for the JX parameter
+    }
+  } else if (server.hasArg("JX")) {  // check if the client sent a value for the JX parameter
     if (server.arg("JX") != "") {
+      command = "";
       joystick_x = server.arg("JX").toInt();
       joystick_y = server.arg("JY").toInt();
-    server.send(200, "text/html", "J");  // Send HTTP status 200 (Ok) and send some text to the browser/client
-    handleJoystick();
+      server.send(200, "text/html", "J");  // Send HTTP status 200 (Ok) and send some text to the browser/client
+      handleJoystick();
     }
   } else {
     handleNotFound();
   }
-
-
 }
 
 void handleNotFound() {
@@ -279,6 +276,12 @@ void BeepHorn() {
   Serial.println("horn");
 }
 
+// function to beep a buzzer
+void BeepHorn_stop() {
+  Serial.println("stop horn");
+}
+
+
 // function to turn on LED
 void TurnLightOn() {
   Serial.println("HS_on");
@@ -291,26 +294,6 @@ void TurnLightOff() {
   Serial.println("HS_off");
   digitalWrite(LED_BUILTIN, HIGH);
 }
-
-
-// void joyStickMove() {
-
-//   // SPEED=((sqrt(pow(xMove,2)+pow(yMove,2)))/100)*1000;
-
-
-//   xPosition = server.arg("param1").toInt();
-//   yPosition = server.arg("param2").toInt();
-
-//   xMove = xPosition - 450;
-//   yMove = 450 - yPosition;
-
-//   calculate_motor_speeds(xMove, yMove);
-
-//   Serial.print("X_move,");
-//   Serial.print(xMove);
-//   Serial.print(",Y_Move,");
-//   Serial.print(yMove);
-// }
 
 
 void motor_dir(bool bIsForward, bool aIsForward)  //b is the left motor, a is right motor
@@ -331,25 +314,66 @@ void motor_dir(bool bIsForward, bool aIsForward)  //b is the left motor, a is ri
     digitalWrite(DIR_A, LOW);
 }
 
+void run_motors(int a_speed, int b_speed)  //b is the left motor, a is right motor
+{
+  //speed can be 0-1000 for each motor
+  //if speed is positive, direction pin is set to LOW
+  //if speed is negative, direction pin is set to HIGH
 
-void move_with_joystick() {
+  Serial.print("R_Speed:");
+  Serial.print(a_speed);
 
-  int xMove = joystick_x - 100; //joystick range is from 0-200 where 100,100 is center. we want to know the offset from center. here we get a range of -100 to 100
-  int yMove = joystick_y - 100; //y is the forward / backward component. here we get a range of -100 to 100
+  if (a_speed < 0)
+  {
+    a_speed = -a_speed;
+    digitalWrite(DIR_A, HIGH);
+    Serial.print(",R_dir:1,");
+  }
+  else
+    digitalWrite(DIR_A, LOW);
+    Serial.print(",R_dir:0,");
 
-  int v = (yMove / 100) * max_linear_speed;
-  int omega = (xMove / 100) * max_angular_speed;
+  Serial.print("L_Speed:");
+  Serial.print(b_speed);
+  if (b_speed < 0)
+  {
+    b_speed = -b_speed;
+    digitalWrite(DIR_B, HIGH);
+    Serial.print(",L_dir:1,");
+  }
+  else
+    digitalWrite(DIR_B, LOW);
+    Serial.println(",L_dir:0");
+ 
+  analogWrite(PWM_A, a_speed);
+  analogWrite(PWM_B, b_speed);
 
-  Serial.println("v");
-  Serial.println(v);
-  Serial.println("omega");
-  Serial.println(omega);
+  
+  
 
-  analogWrite(PWM_A, v - omega);  //right
-  analogWrite(PWM_B, v + omega);  //left
+}
 
-  Serial.println("Right Speed");
-  Serial.println(v - omega);
-  Serial.println("Left Speed");
+void move_with_joystick(int joy_x, int joy_y) {
+
+  int xMove = joy_x - JOYSTICK_RANGE;  //joystick range is from 0-200 where 100,100 is center. we want to know the offset from center. here we get a range of -100 to 100
+  int yMove = joy_y - JOYSTICK_RANGE;  //y is the forward / backward component. here we get a range of -100 to 100
+
+  int v = yMove * max_linear_speed / 100;
+  int omega = xMove * max_angular_speed / 100;
+
+  Serial.print("xm:");
+  Serial.print(xMove);
+  Serial.print(",ym:");
+  Serial.print(xMove);
+  Serial.print(",v:");
+  Serial.print(v);
+  Serial.print(",omega:");
+  Serial.print(omega);
+
+  run_motors(-1*(v + omega), v - omega); //invert left motor
+
+  Serial.print(",Right Speed:");
+  Serial.print(v - omega);
+  Serial.print(",Left Speed:");
   Serial.println(v + omega);
 }
